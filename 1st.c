31cfee876ffd35530e88e23c5287d00b30f3c16e
keshavagarwal670 <agarwalkeshav670@gmail.com>
2023-08-18 14:33:38 +0530

    Questions on 18 Aug 2023

--- 1st.c
+++ 1st.c
@@ -1,9 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int main() {
-    const char *source = "./original";
-    const char *softlink = "./softlink";
-    symlink(source, softlink);
+    // Create a soft link (symbolic link)
+    const char *sourcePath = "./original";
+    const char *softlinkPath = "./softlink";
+    if (symlink("original","softlink" ) == -1) {
+        perror("Error creating soft link");
+        return 1;
+    }
+    printf("Soft link created: %s -> %s\n", softlinkPath, sourcePath);
+
+    // Create a hard link
+    const char *hardlinkPath = "hardlink.txt";
+    if (link("original", "hardlink.txt") == -1) {
+        perror("Error creating hard link");
+        return 1;
+    }
+    printf("Hard link created: %s -> %s\n", "hardlink.txt", "original");
+
+    // Create a FIFO (named pipe)
+    const char *fifoPath = "myfifo";
+    if (mkfifo(fifoPath, 0666) == -1) {
+        perror("Error creating FIFO");
+        return 1;
+    }
+    printf("FIFO created: %s\n", fifoPath);
+
     return 0;
 }
 
