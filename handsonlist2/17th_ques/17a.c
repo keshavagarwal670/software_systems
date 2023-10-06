863f6422f5df6d5d690dc51979ea7dd11e30ee22
keshavagarwal670 <agarwalkeshav670@gmail.com>
2023-10-06 19:58:07 +0530

    17th_ques done

---
+++ handsonlist2/17th_ques/17a.c
@@ -0,0 +1,63 @@
+/*
+============================================================================
+Name : 17a.c
+Author : Keshav Agarwal
+Description : Write a program to execute ls -l | wc.
+              a. use dup
+              b. use dup2
+              c. use fcntl
+Date: 6th oct, 2023.
+============================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    int pipe_fd[2];
+    if (pipe(pipe_fd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t child_pid = fork();
+
+    if (child_pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (child_pid == 0) {
+
+        close(pipe_fd[0]);
+
+        int stdout_copy = dup(STDOUT_FILENO);
+        if (stdout_copy == -1) {
+            perror("dup");
+            exit(EXIT_FAILURE);
+        }
+
+        close(STDOUT_FILENO);
+
+        dup(pipe_fd[1]);
+        close(pipe_fd[1]);
+
+        execlp("ls", "ls", "-l", NULL);
+        perror("execlp");
+        exit(EXIT_FAILURE);
+    } else {
+        close(pipe_fd[1]);
+
+        close(STDIN_FILENO);
+        dup(pipe_fd[0]);
+        close(pipe_fd[0]);
+
+        execlp("wc", "wc", NULL);
+        perror("execlp");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
+
