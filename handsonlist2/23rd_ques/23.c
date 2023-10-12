a8dbf1b10da599049dab4241b1a92cad6e4b622c
keshavagarwal670 <agarwalkeshav670@gmail.com>
2023-10-12 17:40:37 +0530

    done 23,24,25,26,27

---
+++ handsonlist2/23rd_ques/23.c
@@ -0,0 +1,43 @@
+/*
+============================================================================
+Name : 23.c
+Author : Keshav Agarwal
+Description : Write a program to print the maximum number of files can be opened within a process and
+              size of a pipe (circular buffer).
+Date: 12th oct, 2023
+============================================================================
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+
+int main() {
+
+    long max_files = sysconf(_SC_OPEN_MAX);
+    if (max_files == -1) {
+        perror("sysconf");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Maximum number of files a process can open: %ld\n", max_files);
+
+
+    int pipe_fd[2];
+    if (pipe(pipe_fd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    long pipe_size = fpathconf(pipe_fd[0], _PC_PIPE_BUF);
+    if (pipe_size == -1) {
+        perror("fpathconf");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Size of a pipe's circular buffer: %ld bytes\n", pipe_size);
+
+    return 0;
+}
